ec77a5d8b173eb07c2067bc601c532d4edbca327
Priya <guptapriyab@gmail.com>
2020-01-03 11:42:15 +0100

    Updates

---
+++ precache-manifest.a54a3da9707705123bedf16e24dd223e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acc67de46818bb59adf46e0c2a6bef9e",
+    "url": "/Burger-Builder-App/index.html"
+  },
+  {
+    "revision": "4fe79e929dd83198a37f",
+    "url": "/Burger-Builder-App/static/css/main.926299cc.chunk.css"
+  },
+  {
+    "revision": "a9eaeed44e2efc5a0e4c",
+    "url": "/Burger-Builder-App/static/js/2.433f8d0f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Burger-Builder-App/static/js/2.433f8d0f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4fe79e929dd83198a37f",
+    "url": "/Burger-Builder-App/static/js/main.55d8a4d5.chunk.js"
+  },
+  {
+    "revision": "94f03a6c45828e96945f",
+    "url": "/Burger-Builder-App/static/js/runtime-main.69084860.js"
+  }
+]);
