7feb5a0b864129564e70e482d6d8dc40c1a4f0b4
Priya <guptapriyab@gmail.com>
2020-03-07 23:12:31 +0530

    Added logp

--- src/component/Layout/Layout.js
+++ src/component/Layout/Layout.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import styles from './Layout.module.css';
 import Auxillary from '../../hoc/Auxillary';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+
 const Layout = props => {
     console.log(styles);
     return(
     <Auxillary>
-        <div> ToolBar Sidedrawer Backdrop </div>
+        <Toolbar/>
         <main className={styles.content}>
             <div>{props.children}</div>
         </main>
