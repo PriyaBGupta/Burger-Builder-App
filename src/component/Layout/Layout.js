8980bc1f04c87a83fb3cd84e7292a4127392835d
Priya <guptapriyab@gmail.com>
2020-03-10 23:46:54 +0530

    Added hamburger menu

--- src/component/Layout/Layout.js
+++ src/component/Layout/Layout.js
@@ -6,18 +6,20 @@ import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 class Layout extends Component{
     state = {
-        showSideDrawer: true
+        showSideDrawer: false
     }
     sideDrawerClosedHandler = () => {
         this.setState({showSideDrawer: false});
     }
-    toggleMenuHandler = () => {
-        this.setState({showSideDrawer: true});
+    sideDrawerToggleHandler = () => {
+        this.setState((prevState) => {
+            return {showSideDrawer: !prevState.showSideDrawer}
+        });
     }
     render(){
         return(
         <Auxillary>
-            <Toolbar toggledMenu={this.toggleMenuHandler}/>
+            <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler}/>
             <SideDrawer 
             open={this.state.showSideDrawer} 
             closed={this.sideDrawerClosedHandler}/>
--- src/component/Navigation/Toolbar/Toolbar.js
+++ src/component/Navigation/Toolbar/Toolbar.js
@@ -2,10 +2,11 @@ import React from 'react';
 import classes from './Toolbar.module.css';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
 
 const Toolbar = (props) => (
     <header className={classes.Toolbar}>
-        <div onClick={props.toggledMenu}>MENU</div>
+        <DrawerToggle clicked={props.drawerToggleClicked}/>
         <Logo/>
         <nav className={classes.DesktopOnly}>
             <NavigationItems/>
