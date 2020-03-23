106ea81bc39d822b47edd3d1ce1da68ce72ef41c
Priya <guptapriyab@gmail.com>
2020-03-23 23:52:25 +0530

    Outputting Orders

--- src/container/Orders/Orders.js
+++ src/container/Orders/Orders.js
@@ -1,13 +1,46 @@
 import React, { Component } from 'react';
-import Order from '../../component/Order/Order'
-class Orders extends Component{
-    render(){
-        return(
+import Order from '../../component/Order/Order';
+import axios from '../../axios-orders';
+import withErrorHandling from '../../hoc/withErrorHandler/withErrorHandler';
+
+class Orders extends Component {
+    state = {
+        orders: [],
+        loading: false
+    }
+    componentDidMount() {
+        axios.get('/orders.json')
+            .then(res => {
+                const fetchOrders = [];
+                this.setState({loading: false});
+                //res.data is object whose first property is unique identifier 
+                for (let key in res.data) {
+                    fetchOrders.push({
+                        ...res.data[key],
+                        id: key
+                    });
+                }
+                this.setState({orders: fetchOrders});
+            })
+            .catch(err => {
+                this.setState({loading: false})
+            });
+
+    }
+    render() {
+        if(this.state.loading){
+
+        }
+        return (
             <div>
-                <Order/>
-                <Order/>
+                {this.state.orders.map((order)=>(
+                    <Order 
+                        key={order.id}
+                        ingredients={order.ingredients}
+                        price={+order.price}/>
+                ))}
             </div>
         )
     }
 }
-export default Orders;
+export default withErrorHandling(Orders, axios);
