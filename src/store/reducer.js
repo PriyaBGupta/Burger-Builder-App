eb3b0a67d3d6e34278590ac782859b03f5d55b5b
Priya <guptapriyab@gmail.com>
2020-04-05 18:13:00 +0530

    Redux React basic setup added

---
+++ src/store/reducer.js
@@ -0,0 +1,12 @@
+import * as actionType from './action';
+const initialState = {
+    ingredients: null,
+    totalPrice: 4,
+}
+const reducer = (state = initialState, action) => {
+    // switch(action.type){
+    //     case actionType.ADD_INGREDIENT:
+
+    // }
+}
+export default reducer;
