18d7622a6002da4972f98385ed0f754ce8bef88a
Priya <guptapriyab@gmail.com>
2020-04-09 20:58:13 +0530

    Added Redux tool extension the basic one

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import {BrowserRouter} from 'react-router-dom';
 import {createStore} from 'redux';
 import reducer from './store/reducer';
 import {Provider} from 'react-redux';
-const store = createStore(reducer);
+const store = createStore(reducer,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 const app = (
     //Provider wraps everthing
     <Provider store={store}>
