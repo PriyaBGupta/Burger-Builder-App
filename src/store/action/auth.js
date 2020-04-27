50608eb3ac07dcc73566ca3bd36654504651d268
Priya <guptapriyab@gmail.com>
2020-04-27 14:36:54 +0530

    Added redux saga and also made saga file

--- src/store/action/auth.js
+++ src/store/action/auth.js
@@ -25,6 +25,7 @@ export const authStart = () => {
 export const authLogout = () => {
     localStorage.removeItem('token');
     localStorage.removeItem('expirationDate');
+    localStorage.removeItem('userId');
     return{
         type: actionTypes.AUTH_LOGOUT
     }
